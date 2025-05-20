8dc9fa4f54d63e98f1feec04e127a24b83fa315d
Ian Meinert <marinesct@gmail.com>
2025-05-19 23:07:15 -0500

    added gemini langchain

--- lang-chain-mcp-adapters/main.py
+++ lang-chain-mcp-adapters/main.py
@@ -2,9 +2,19 @@ import asyncio
 import os
 
 from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_mcp_adapters.tools import load_mcp_tools
+from langgraph.prebuilt import create_react_agent
+from mcp import ClientSession, StdioServerParameters
+from mcp.client.stdio import stdio_client
 
 load_dotenv()
-print(os.getenv("GOOGLE_API_KEY"))
+llm = ChatGoogleGenerativeAI(model="gemini-1.5-pro")
+
+stdio_server_params = StdioServerParameters(
+    command="python",
+    args=["C:\\Users\\marin\\Documents\\Computers\\DeveloperPortal\\mcp-servers\\lang-chain-mcp-adapters\\math.py"],
+)
 
 async def main():
     print("Hello from lang-chain-mcp-adapters!")
